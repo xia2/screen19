76f41846098f4cb4c17b6955d6df0ff6ea7ccf1e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-11-19 15:18:50 +0000

    simple screening script

--- command_line/screen.py
+++ command_line/screen.py
@@ -17,35 +17,24 @@ Examples::
 
 '''
 
-def _prettyprint_dictionary(d):
-  return "{\n%s\n}" % \
-    "\n".join(["  %s: %s" % (k, str(d[k]).replace("\n", "\n%s" % (" " * (4 + len(k)))))
-      for k in d.iterkeys() ])
+from logging import info, debug, warn
 
-def run(args):
-  if len(args) == 0:
-    print help_message
-    sys.exit(0)
-
+class i19Screen():
   import libtbx.load_env
   from libtbx.utils import Sorry
-  from dials.util import log
-  from logging import info, debug, warn
 
-  # Configure the logging
-  log.config(1, info='i19.screen.log', debug='i19.screen.debug.log')
+  def _prettyprint_dictionary(self, d):
+    return "{\n%s\n}" % \
+      "\n".join(["  %s: %s" % (k, str(d[k]).replace("\n", "\n%s" % (" " * (4 + len(k)))))
+        for k in d.iterkeys() ])
 
-  from dials.util.version import dials_version
-  from i19.util.version import i19_version
-  info("%s using %s" % (i19_version(), dials_version()))
-
-  if len(args) != 1 or not args[0].endswith('.json'):
+  def _import(self, files):
     info("\nImporting data...")
-    command = [ "dials.import" ] + args
+    command = [ "dials.import" ] + files
     debug("running %s" % " ".join(command))
 
     result = run_process(command, print_stdout=False)
-    debug("result = %s" % _prettyprint_dictionary(result))
+    debug("result = %s" % self._prettyprint_dictionary(result))
 
     if result['exitcode'] == 0:
       info("Successfully completed (%.1f sec)" % result['runtime'])
@@ -53,17 +42,109 @@ def run(args):
       warn("Failed with exit code %d" % result['exitcode'])
       sys.exit(1)
 
-  info("\nTesting pixel intensities...")
+  def _count_processors(self):
+    command = [ "libtbx.show_number_of_processors" ]
+    debug("running %s" % command)
+    result = run_process(command, print_stdout=False)
+    debug("result = %s" % self._prettyprint_dictionary(result))
+    if result['exitcode'] == 0:
+      self.nproc = result['stdout']
+    else:
+      warn("Could not determine number of available processors. Error code %d" % result['exitcode'])
+      sys.exit(1)
 
-  info("\nSpot finding...")
+  def _check_intensities(self):
+    info("\nTesting pixel intensities...")
+    command = [ "xia2.overload", self.json_file ]
+    result = run_process(command, print_stdout=False)
+    debug("result = %s" % self._prettyprint_dictionary(result))
 
-  info("\nIndexing...")
+    if result['exitcode'] == 0:
+      print "Pixel intensity distribution:"
+      for l in result['stdout'].split("\n"):
+        if not l.endswith(': 0') and not l == "":
+          info(l)
+      info("Successfully completed (%.1f sec)" % result['runtime'])
+    else:
+      warn("Failed with exit code %d" % result['exitcode'])
+      sys.exit(1)
 
-  info("\nRefining bravais settings...")
+  def _find_spots(self):
+    info("\nSpot finding...")
+    command = [ "dials.find_spots", self.json_file, "nproc=%s" % self.nproc ]
+    result = run_process(command, print_stdout=False)
+    debug("result = %s" % self._prettyprint_dictionary(result))
+    if result['exitcode'] == 0:
+      import re
+      m = re.search('Saved ([0-9]+) reflections to strong.pickle', result['stdout'])
+      info("Found %s reflections (%.1f sec)" % (m.group(1), result['runtime']))
+    else:
+      warn("Failed with exit code %d" % result['exitcode'])
+      sys.exit(1)
 
-  return
+  def _index(self):
+    info("\nIndexing...")
+    command = [ "dials.index", self.json_file, "strong.pickle" ]
+    result = run_process(command, print_stdout=False)
+    debug("result = %s" % self._prettyprint_dictionary(result))
+    if result['exitcode'] == 0:
+      import re
+      m = re.search('model [0-9]+ \(([0-9]+) [^\n]*\n[^\n]*\n[^\n]*Unit cell: \(([^\n]*)\)\n[^\n]*Space group: ([^\n]*)\n', result['stdout'])
+      info("Found primitive solution: %s (%s) using %s reflections" % (m.group(3), m.group(2), m.group(1)))
+      info("Successfully completed (%.1f sec)" % result['runtime'])
+    else:
+      warn("Failed with exit code %d" % result['exitcode'])
+      sys.exit(1)
 
+  def _refine_bravais(self):
+    info("\nRefining bravais settings...")
+    command = [ "dials.refine_bravais_settings", "experiments.json", "indexed.pickle" ]
+    result = run_process(command, print_stdout=False)
+    debug("result = %s" % self._prettyprint_dictionary(result))
+    if result['exitcode'] == 0:
+      import re
+      m = re.search('---+\n[^\n]*\n---+\n(.*\n)*---+', result['stdout'])
+      info(m.group(0))
+      info("Successfully completed (%.1f sec)" % result['runtime'])
+    else:
+      warn("Failed with exit code %d" % result['exitcode'])
+      sys.exit(1)
+
+  def run(self, args):
+    if len(args) == 0:
+      print help_message
+      return
+
+    # Configure the logging
+    from dials.util import log
+    log.config(1, info='i19.screen.log', debug='i19.screen.debug.log')
+
+    from dials.util.version import dials_version
+    from i19.util.version import i19_version
+    info("%s using %s" % (i19_version(), dials_version()))
+
+    self._count_processors()
+
+    if len(args) == 1 and args[0].endswith('.json'):
+      self.json_file = args[0]
+    else:
+      self._import(args)
+      self.json_file = 'datablock.json'
+
+    if True:
+      self._check_intensities()
+
+    if True:
+      self._find_spots()
+
+    if True:
+      self._index()
+
+    if True:
+      self._refine_bravais()
+
+    return
 
 if __name__ == '__main__':
   import sys
-  run(sys.argv[1:])
+  i19Screen().run(sys.argv[1:])
