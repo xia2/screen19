bc17442f9fdebede994b6431f8c9855679b4debc
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-11-20 11:39:34 +0000

    clean up

--- command_line/screen.py
+++ command_line/screen.py
@@ -1,5 +1,8 @@
 from __future__ import division
+from logging import info, debug, warn
+import re
 import sys
+
 from i19.util.procrunner import run_process
 
 help_message = '''
@@ -16,8 +19,6 @@ Examples::
   i19.screen /path/to/data/
 
 '''
-
-from logging import info, debug, warn
 
 class i19Screen():
   import libtbx.load_env
@@ -61,7 +62,6 @@ class i19Screen():
 
     if result['exitcode'] == 0:
       print "Pixel intensity distribution:"
-      import re
       hist = {}
       for l in result['stdout'].split("\n"):
         m = re.search('^([0-9]+) - [0-9]+: ([0-9]+)$', l)
@@ -118,7 +118,6 @@ class i19Screen():
     result = run_process(command, print_stdout=False)
     debug("result = %s" % self._prettyprint_dictionary(result))
     if result['exitcode'] == 0:
-      import re
       m = re.search('Saved ([0-9]+) reflections to strong.pickle', result['stdout'])
       info("Found %s reflections (%.1f sec)" % (m.group(1), result['runtime']))
     else:
@@ -131,7 +130,6 @@ class i19Screen():
     result = run_process(command, print_stdout=False)
     debug("result = %s" % self._prettyprint_dictionary(result))
     if result['exitcode'] == 0:
-      import re
       m = re.search('model [0-9]+ \(([0-9]+) [^\n]*\n[^\n]*\n[^\n]*Unit cell: \(([^\n]*)\)\n[^\n]*Space group: ([^\n]*)\n', result['stdout'])
       info("Found primitive solution: %s (%s) using %s reflections" % (m.group(3), m.group(2), m.group(1)))
       info("Successfully completed (%.1f sec)" % result['runtime'])
@@ -145,7 +143,6 @@ class i19Screen():
     result = run_process(command, print_stdout=False)
     debug("result = %s" % self._prettyprint_dictionary(result))
     if result['exitcode'] == 0:
-      import re
       m = re.search('---+\n[^\n]*\n---+\n(.*\n)*---+', result['stdout'])
       info(m.group(0))
       info("Successfully completed (%.1f sec)" % result['runtime'])
@@ -174,20 +171,12 @@ class i19Screen():
       self._import(args)
       self.json_file = 'datablock.json'
 
-    if True:
-      self._check_intensities()
-
-    if True:
-      self._find_spots()
-
-    if True:
-      self._index()
-
-    if True:
-      self._refine_bravais()
+    self._check_intensities()
+    self._find_spots()
+    self._index()
+    self._refine_bravais()
 
     return
 
 if __name__ == '__main__':
-  import sys
   i19Screen().run(sys.argv[1:])
