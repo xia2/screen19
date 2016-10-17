e5ee7865d3758d0a0e5ecef0daa5cd0aad87b22a
graeme-winter <graeme.winter@gmail.com>
2016-10-17 10:59:32 +0100

    Proper test, evaluation of scaling factor etc.

--- util/time_analysis.py
+++ util/time_analysis.py
@@ -2,14 +2,14 @@
 from __future__ import division
 from scitbx.array_family import flex
 
-def source(length=10000, f_hz=100):
+def source(length=10000, sample_rate_hz=100, f_hz=10):
   import math
   import random
 
   data = flex.double(length, 0.0)
 
   for j in range(length):
-    data[j] = 10 * math.sin(2 * math.pi * j * (f_hz / length))
+    data[j] = 10 * math.sin(2 * math.pi * j * (f_hz / sample_rate_hz))
     data[j] += random.random()
 
   return data
@@ -22,16 +22,22 @@ def fft(d):
     _d[j] = d[j]
   t = f.forward(_d)
   p = flex.abs(t) ** 2
+
+  # remove the DC component
   p[0] = 0.0
+
   return p
 
 def test():
-  f_hz = [100, 200, 500, 100, 50]
+  sample_rate_hz = [100, 200, 500, 100, 50]
   length = [10000, 10000, 10000, 1000, 2000]
-  for l, f in zip(length, f_hz):
-    d = source(length=l, f_hz=f)
+  f_hz = [5, 10, 20, 20, 4]
+  for l, s, f in zip(length, sample_rate_hz, f_hz):
+    f_scale = s / l
+    d = source(length=l, sample_rate_hz=s, f_hz=f)
     d = fft(d)
-    assert(flex.max(d) == d[f])
+    assert(flex.max(d) == d[int(round(f / f_scale))])
+  print 'OK'
 
 if __name__ == '__main__':
   test()
