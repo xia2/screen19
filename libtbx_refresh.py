54dfd889489625d76a8eddc1b3a96c49beabb560
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-09-28 09:53:14 +0100

    define dependencies

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -5,3 +5,10 @@ try:
   print version.i19_version()
 except ImportError:
   pass
+
+try:
+  import libtbx.pkg_utils
+  libtbx.pkg_utils.require('mock', '>=2.0')
+  libtbx.pkg_utils.require('pytest', '>=2')
+except ImportError:
+  print "\n" * 10 + "Could not verify dependencies: cctbx sources out of date" + "\n" * 10
