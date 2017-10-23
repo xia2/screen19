83ec1764106eb75246493c78449718d961d1c535
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-10-23 13:35:13 +0100

    pytest compatibility stuff moved to libtbx :thumbsup:

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,8 @@
-from __future__ import division
-import libtbx.load_env
-from libtbx import test_utils
+from __future__ import absolute_import, division, print_function
 from libtbx.test_utils.pytest import discover
 
 tst_list = [
 #  "$D/tests/tst_legacy.py",
   ["$D/tests/tst_legacy_mult.py", "1"]
 #  ["$D/tests/tst_legacy_mult.py", "2"]
-] + discover("i19")
-
-if (__name__ == "__main__"):
-  build_dir = libtbx.env.under_build("i19")
-  dist_dir = libtbx.env.dist_path("i19")
-  test_utils.run_tests(build_dir, dist_dir, tst_list)
+] + discover()
