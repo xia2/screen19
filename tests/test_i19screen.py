d92c545e34685e1414730a1aa109b1cc5929be28
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-09-25 12:40:19 +0100

    Update test to use regression_data

--- tests/test_i19screen.py
+++ tests/test_i19screen.py
@@ -6,19 +6,12 @@ from i19.command_line.screen import i19_screen
 def test_i19screen_command_line_help_does_not_crash():
   i19_screen().run('')
 
-def test_i19screen(tmpdir):
-  import os
-  import libtbx
-  xia2_regression = libtbx.env.under_build("xia2_regression")
-  data_dir = os.path.join(xia2_regression, "test_data", "X4_wide")
-  if not os.path.exists(data_dir):
-    pytest.skip('Skipping test: xia2_regression data not found')
+def test_i19screen(regression_data, run_in_tmpdir):
+  data_dir = regression_data('X4_wide').strpath
 
-  tmpdir.chdir()
   i19_screen().run([data_dir])
 
-  with tmpdir.join('i19.screen.log').open() as fh:
-    logfile = fh.read()
+  logfile = run_in_tmpdir.join('i19.screen.log').read()
 
   assert 'i19.screen successfully completed' in logfile
   assert 'photon incidence rate is outside the linear response region' in logfile
