b7a0653cdb2c20def38a687963763b75455ebbcb
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-11-06 08:26:00 +0000

    Add --regression command line option

--- conftest.py
+++ conftest.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import, division, print_function
 
-from dials.conftest import regression_data, run_in_tmpdir
+from dials.conftest import pytest_addoption, regression_data, run_in_tmpdir
