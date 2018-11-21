8aaeae23db31162677637a41c5343558f3842450
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-11-21 13:16:45 +0000

    Add test for template parsing

--- tests/test_i19screen.py
+++ tests/test_i19screen.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+import mock
 import pytest
 from i19.command_line.screen import I19Screen
 
@@ -15,3 +16,21 @@ def test_i19screen(regression_data, run_in_tmpdir):
 
   assert 'i19.screen successfully completed' in logfile
   assert 'photon incidence rate is outside the linear response region' in logfile
+
+@mock.patch('i19.command_line.screen.procrunner')
+def test_i19screen_calls(procrunner, run_in_tmpdir):
+  procrunner.run.return_value = {'exitcode': 0, 'runtime': 0}
+  files = 'dataset.cbf:1:100'
+
+  with pytest.raises(SystemExit):
+    I19Screen().run([files])
+
+  procrunner.run.assert_called_once_with([
+          'dials.import',
+          'input.template=dataset.cbf',
+          'geometry.scan.image_range=1,100',
+          'geometry.scan.extrapolate_scan=True'
+      ],
+      debug=False,
+      print_stdout=False,
+  )
