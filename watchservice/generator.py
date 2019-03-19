b6fd096ed7ccbc395bac0f614568951fc628757b
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-03-19 10:39:30 +0000

    Clean up
    Remove unused code,
    reorganize used code

--- watchservice/generator.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-import itertools
-
-
-def Generator(pattern, begin, end):
-    """A generator for monotonically increasing filenames following a predifined
-     pattern."""
-    return itertools.imap(lambda i: pattern % i, xrange(begin, end + 1))
