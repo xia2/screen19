36ba8cf98748e101ba81b4a591e37b9d5dc2a825
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2019-11-13 15:57:17 +0000

    Update pytest configuration for dials_data

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,8 @@ from __future__ import absolute_import, division, print_function
 import pytest
 
 try:
-    from dials_data import *
+    import dials_data as _  # noqa: F401
 except ImportError:
-
-    @pytest.fixture
+    @pytest.fixture(scope="session")
     def dials_data():
         pytest.skip("Test requires python package dials_data")
