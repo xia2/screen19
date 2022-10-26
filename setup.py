6a7db0bf84f7e9ffa6f96c13fcd68640a658cf7d
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2022-10-26 15:12:50 +0100

    Setuptools nonsense...

--- setup.py
+++ setup.py
@@ -19,19 +19,25 @@ setup(
     author_email="scientificsoftware@diamond.ac.uk",
     description="Screening program for small-molecule single-crystal X-ray diffraction "
     "data",
+    package_dir={"": "src"},
     entry_points={
-        "console_scripts": [],
-        "libtbx.dispatcher.script": [],
+        "console_scripts": [
+            "screen19 = screen.screen:main"
+            "screen19.minimum_exposure = screen.minimum_exposure:main"
+        ],
+        "libtbx.dispatcher.script": [
+            "screen19.minimum_exposure = screen19.minimum_exposure"
+        ],
         "libtbx.precommit": ["screen19 = screen19"],
     },
-    install_requires=['typing;python_version<"3.5"', "procrunner"],
+    install_requires=['python_version>="3.8"'],
     license="BSD license",
     long_description="\n\n".join([readme, changelog_header, changelog]),
     include_package_data=True,
     name="screen19",
-    packages=find_packages(),
+    packages=find_packages(where="src/screen"),
     test_suite="tests",
-    tests_require=["mock>=2.0", "pytest>=4.5"],
+    tests_require=["pytest"],
     version="0.213",
     zip_safe=False,
 )
