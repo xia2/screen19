9f2f58df0827079d57115ee338fb650afdd9f9f9
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-10-26 16:43:52 +0100

    Get correct versioning from setup.cfg

---
+++ src/screen/__init__.py
@@ -0,0 +1,6 @@
+"""Screening program for small-molecule single-crystal X-ray diffraction data"""
+
+__author__ = "Diamond Light Source - Scientific Software"
+__email__ = "data_analysis@diamond.ac.uk"
+__version__ = "0.213"
+__version_tuple__ = tuple(int(x) for x in __version__.split("."))
